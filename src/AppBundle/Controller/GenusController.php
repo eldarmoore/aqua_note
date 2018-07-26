92fa81595aad0e4d1090293ccb409586cc288b93
Eldar <eldar@kaveret.biz>
2018-07-26 16:13:41 +0300

    Entities, Twig and the Magic dot Syntax

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -41,7 +41,10 @@ class GenusController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $genuses = $em->getRepository('AppBundle:Genus')->findAll();
-        dump($genuses);die;
+        // dump($genuses);die;
+
+        return $this->render('genus/list.html.twig', ['genuses'=> $genuses] );
+
     }
 
     /**
