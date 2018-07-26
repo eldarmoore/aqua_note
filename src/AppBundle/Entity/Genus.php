69af6a34e85772a57131a7d52fbb8c995a97301f
Eldar <eldar@kaveret.biz>
2018-07-26 14:22:47 +0300

    Creating an Entity Class "You're going to hear this word - entity - a lot with Doctrine. Entity - it sounds like an alien parasite."

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Eldar
+ * Date: 7/26/2018
+ * Time: 2:17 PM
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus")
+ */
+class Genus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+}
