d848db23fb77466ea125838ddfa1b7932974cd4e
Eldar <eldar@kaveret.biz>
2018-07-26 09:50:25 +0300

    JSON Responses + Route Generation

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -8,8 +8,10 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
@@ -19,12 +21,30 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
+        return $this->render('genus/show.html.twig', ['name' => $genusName,'welcome' => 'hello world!']);
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes")
+     * @Method("GET")
+     */
+    public function getNotesAction()
+    {
         $notes = [
-            'Octopus asked me a riddle, outsmarted me',
-            'I counted 8 legs... as they wrapped around me',
-            'Inked!'
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
         ];
 
-        return $this->render('genus/show.html.twig', ['name' => $genusName,'notes' => $notes,'welcome' => 'hello world!']);
+        $data = [
+            'notes' => $notes,
+        ];
+
+        // return new Response(json_encode($data));
+
+        // What this does
+        // 1. It calls json_encode
+        // 2. It sets application/json content type header on the response
+        return new JsonResponse($data);
     }
 }
