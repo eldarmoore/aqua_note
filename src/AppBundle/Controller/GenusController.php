7254eeb41dc5a10cb389dd6f0723c785f3ca8f1a
Eldar <eldar@kaveret.biz>
2018-07-26 15:58:48 +0300

    Query for a List of Genuses

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -35,6 +35,16 @@ class GenusController extends Controller
     }
 
     /**
+     * @Route("/genus")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $genuses = $em->getRepository('AppBundle:Genus')->findAll();
+        dump($genuses);die;
+    }
+
+    /**
      * @Route("/genus/{genusName}")
      */
     public function showAction($genusName)
