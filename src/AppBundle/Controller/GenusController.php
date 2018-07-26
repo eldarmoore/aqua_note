ad5f77f13072a38002bce1019f9ffed2fc1d1705
Eldar <eldar@kaveret.biz>
2018-07-26 09:08:17 +0300

    Twig: For a Good time with Templates.

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -19,6 +19,12 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-        return $this->render('genus/show.html.twig', ['name' => $genusName]);
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+        ];
+
+        return $this->render('genus/show.html.twig', ['name' => $genusName,'notes' => $notes,'welcome' => 'hello world!']);
     }
 }
