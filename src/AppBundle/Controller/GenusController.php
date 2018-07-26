bc4d978fd7b2ac78dc681a5546791012db34bfea
Eldar <eldar@kaveret.biz>
2018-07-26 10:30:56 +0300

    Using a Service

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -21,7 +21,11 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-        return $this->render('genus/show.html.twig', ['name' => $genusName,'welcome' => 'hello world!']);
+        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+
+        $funFact = $this->get('markdown.parser')->transform($funFact);
+
+        return $this->render('genus/show.html.twig', ['name' => $genusName,'welcome' => 'hello world!','funFact' => $funFact]);
     }
 
     /**
